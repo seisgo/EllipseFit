cad43efa85ef9589d45552c6fe23b46412f7d2a3
Zhenyu <yuanzhenyu@live.com>
2016-08-15 14:32:48 +0800

    Add C++ implementation searching index

---
+++ Index.cpp
@@ -0,0 +1,3 @@
+//Index file for C++ implementation of ellipse fitting
+//This file could be used to sign this repository as C++ format for searching
+//The realistic implementation of C++ is in C++ directory
