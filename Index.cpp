73ee0d92d2c1fdbb9825f4ad00bb5c0abb819ed5
Zhenyu <yuanzhenyu@live.com>
2016-08-23 09:10:58 +0800

    Delete Index.cpp

--- Index.cpp
+++
@@ -1,3 +0,0 @@
-//Index file for C++ implementation of ellipse fitting
-//This file could be used to sign this repository as C++ format for searching
-//The realistic implementation of C++ is in C++ directory
